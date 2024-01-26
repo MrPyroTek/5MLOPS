f4ef730ab5f1e259d72b10c4a160001a719c56e6
thomas <thomasangama77460@gmail.com>
2024-01-26 18:09:06 +0100

    feat(script): add conf on train, evaluate and predict for prefect

--- src/evaluate.py
+++ src/evaluate.py
@@ -1,7 +1,9 @@
 import numpy as np
 
 from sklearn.metrics import f1_score
+from prefect import task
 
+@task(name="Evaluation", tags=["Model"])
 def evaluate(y_real: np.array, y_pred: np.array)-> float:
     """Calculate f1 score from real target value and prediction value"""
     
---
+++ src/train_predict.py
@@ -0,0 +1,20 @@
+import pandas as pd
+
+from prefect import flow
+
+from evaluate import evaluate
+from train_model import trainModel
+from predict import predict
+
+@flow(name="Model initialisation")
+def train_and_predict(
+        x_train: pd.DataFrame,
+        y_train: pd.DataFrame,
+        x_test: pd.DataFrame,
+        y_test: pd.DataFrame
+) -> dict:
+    """Train model, predict values and calculate error"""
+    model = trainModel(x_train, y_train)
+    prediction = predict(x_test, model)
+    score_f1 = evaluate(y_test, prediction)
+    return {'model': model, 'score_f1': score_f1}

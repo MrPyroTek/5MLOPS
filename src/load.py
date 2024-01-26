1588a6bb0647ec87a8d4bb136e04296fc688e1f3
Théo FRERE <theo.frere@umanis.com>
2024-01-26 14:39:41 +0100

    Feature load csv
    Co-authored-by: BenjaminFourmaux <benjamin.fourmauxb@gmail.com>
    Co-authored-by: totolamatrac <thomasangama77460@gmail.com>

---
+++ src/load.py
@@ -0,0 +1,9 @@
+import pandas as pd
+from prefect import task
+
+
+@task(name='load_csv', tags=['preprocessing'])
+def load_csv(
+        path: str = "data/HeartDisease.csv",
+) -> pd.DataFrame:
+    return pd.read_csv(path, index_col=0)

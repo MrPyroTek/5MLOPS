bd7db13111200772b8caaff6ba7c723a8139d948
Benjamin Fourmaux <benjamin.fourmauxb@gmail.com>
2024-01-26 11:28:07 +0100

    fix input format

--- API/lib/preprocessing.py
+++ API/lib/preprocessing.py
@@ -2,4 +2,5 @@ import pandas as pd
 
 
 def prepare_data(data: dict) -> pd.DataFrame:
-    return pd.DataFrame(data=data)
+    index = ["age", "sex", "cp", "trestbps", "chol", "fbs", "restecg", "thalach", "exang", "oldpeak", "slope", "ca", "thal"]
+    return pd.DataFrame(data=data, index=index)

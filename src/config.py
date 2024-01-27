49a2077292be521183df2e0b6d69a0f78ab9140e
thomas <thomasangama77460@gmail.com>
2024-01-27 17:12:50 +0100

    feat: add build_model_mlflow

--- src/config.py
+++ src/config.py
@@ -5,10 +5,12 @@ ROOT_DIR = Path(__file__).parent
 PATH_LOCAL_DATA = ROOT_DIR / "local_data"
 
 # DATA
-CSV_DATA_PATH = PATH_LOCAL_DATA / "HeartDisease.csv"
+CSV_DATA_PATH = PATH_LOCAL_DATA / "HeartDiseaseUCI.csv"
 
 # MLFLOW
 MLFLOW_TRACKING_URI = "http://mlflow:5000"
+MLFLOW_EXPERIMENT_NAME = "heart_disease_status"
+REGISTERED_MODEL_NAME = "heart_disease_model"
 
 # MISC
 NUMERICAL_VARS = ['age', 'trestbps', 'chol', 'thalach', 'oldpeak', 'ca']

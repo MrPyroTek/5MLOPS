a2afb0c9cd5c1d9d6523696d22524ec2e1e82cfe
thomas <thomasangama77460@gmail.com>
2024-01-27 12:49:05 +0100

    fix(load.py): fix index of return dataframe

--- src/load.py
+++ src/load.py
@@ -4,6 +4,7 @@ from prefect import task
 
 @task(name='load_csv', tags=['preprocessing'])
 def load_csv(
-        path: str = "data/HeartDisease.csv",
+        path: str,
 ) -> pd.DataFrame:
-    return pd.read_csv(path, index_col=0)
+    df = pd.read_csv(path, index_col=0).reset_index(drop=True)
+    return df

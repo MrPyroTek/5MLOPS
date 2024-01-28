d00930cf3b76e798388ed03f83d80dea9dbb0b3a
BenjaminFourmaux <benjamin.fourmauxb@gmail.com>
2024-01-28 16:49:09 +0100

    Add preprocessing pipeline in API

--- API/app_config.py
+++ API/app_config.py
@@ -2,11 +2,18 @@
 APP_VERSION = "0.0.1"
 
 # MODELS
-MODEL_NAME = "heart_disease_classifier"
+MODEL_NAME = "heart_disease_model"
 MODEL_VERSION = "0.0.1"
 REPO_URL = "http://localhost:8000/"
-PATH_TO_MODEL = f"{REPO_URL}models/{MODEL_NAME}/production"
+PATH_TO_MODEL = f"{REPO_URL}models/{MODEL_NAME}/Production"
 
 # API
 APP_TITLE = "HeartDiseasePredictionApp"
-APP_DESCRIPTION = "A simple API to predict if a patient has a heart disease "
+APP_DESCRIPTION = "A simple API to predict if a patient has a heart disease"
+
+# DATA PREPROCESS
+DATA_INDEX = ["age", "sex", "cp", "trestbps", "chol", "fbs", "restecg", "thalach", "exang", "oldpeak", "slope", "ca",
+              "thal"]
+NUMERICAL_VARS = ['age', 'trestbps', 'chol', 'thalach', 'oldpeak', 'ca']
+CATEGORICAL_VARS = ['sex', 'cp', 'fbs', 'restecg', 'exang', 'slope', 'thal']
+TARGET_NAME = "potential"
--- API/lib/preprocessing.py
+++ API/lib/preprocessing.py
@@ -1,6 +1,75 @@
+import numpy as np
 import pandas as pd
+from sklearn.pipeline import Pipeline
+from app_config import DATA_INDEX, CATEGORICAL_VARS, NUMERICAL_VARS, TARGET_NAME
+from sklearn.impute import SimpleImputer
+from sklearn.preprocessing import OneHotEncoder
 
 
 def prepare_data(data: dict) -> pd.DataFrame:
-    index = ["age", "sex", "cp", "trestbps", "chol", "fbs", "restecg", "thalach", "exang", "oldpeak", "slope", "ca", "thal"]
-    return pd.DataFrame(data=data, index=index)
+
+    # Preprocessing pipeline
+    df = to_dataframe(data)
+    df = compute_target(df)
+    df = numeric_imputer(df, NUMERICAL_VARS)
+    df = categorical_imputer(df, CATEGORICAL_VARS)
+    df = categorical_encoder(df, CATEGORICAL_VARS)
+
+    return df
+
+
+def to_dataframe(data: dict) -> pd.DataFrame:
+    return pd.DataFrame(data=data, index=DATA_INDEX)
+
+
+def compute_target(df: pd.DataFrame) -> pd.DataFrame:
+    """
+    Cree un Yhat binaire correspondant a la potentielle présence ou non d'une maladie cardiaque
+    """
+    df["potential"] = np.where(df["num"] >= 1, 1, 0)
+    df = df.drop(["num"], axis=1)
+    return df
+
+
+def numeric_imputer(df: pd.DataFrame, numerical_features: List[str]) -> pd.DataFrame:
+    """
+    Impute les valeurs manquantes pour les valeurs numeriques
+    """
+    numeric_transformer = Pipeline(steps=[("imputation", SimpleImputer(strategy="mean"))])
+
+    df[numerical_features] = numeric_transformer.fit_transform(df[numerical_features])
+
+    return df
+
+
+def categorical_imputer(df: pd.DataFrame, categorical_features: List[str]) -> pd.DataFrame:
+    """
+    Impute les valeurs manquantes pour les valeurs categoriques
+    """
+    categorical_transformer = Pipeline(steps=[
+        ("imputation", SimpleImputer(strategy="most_frequent"))
+    ])
+
+    df[categorical_features] = categorical_transformer.fit_transform(df[categorical_features])
+
+    return df
+
+
+def categorical_encoder(df: pd.DataFrame, categorical_features: List[str]) -> pd.DataFrame:
+    categorical_transformer = Pipeline(steps=[
+        ("imputation", SimpleImputer(strategy="most_frequent")),
+        ("encoder", OneHotEncoder())
+    ])
+    """
+    Encode les valeurs categoriques
+    """
+    df_encoded = categorical_transformer.fit_transform(df[categorical_features])
+
+    df = pd.concat([df, pd.DataFrame(df_encoded.toarray(),
+                                     columns=categorical_transformer.named_steps['encoder'].get_feature_names_out(
+                                         categorical_features))], axis=1)
+
+    # Drop the original categorical features
+    df = df.drop(categorical_features, axis=1)
+
+    return df
